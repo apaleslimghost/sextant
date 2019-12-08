2e08df676b1d4414b8e0dffda438c7d30c4a6d08
bren <bren@153.io>
2019-12-08 20:26:23 +0000

    split out gradient mapper

--- components/tile.jsx
+++ components/tile.jsx
@@ -1,22 +1,7 @@
 import React from 'react'
 import Canvas from './canvas'
 import { Simplex2 } from 'tumult'
-
-const gradientCanvas = document.createElement('canvas')
-gradientCanvas.width = 255
-gradientCanvas.height = 1
-
-const gradientCtx = gradientCanvas.getContext('2d')
-const gradient = gradientCtx.createLinearGradient(0, 0, 255, 0)
-gradient.addColorStop(0, 'blue')
-gradient.addColorStop(0.5, 'blue')
-gradient.addColorStop(0.501, 'green')
-gradient.addColorStop(1, 'green')
-
-gradientCtx.fillStyle = gradient
-gradientCtx.fillRect(0, 0, 255, 1)
-
-const gradientData = gradientCtx.getImageData(0, 0, 255, 1).data
+import gradientMap from './gradientmap'
 
 export default ({ }) => <Canvas draw={(ctx, canvas) => {
 	const noise = new Simplex2('seed')
@@ -28,15 +13,23 @@ export default ({ }) => <Canvas draw={(ctx, canvas) => {
 		for (let x = 0; x < width; x++) {
 			const i = (x + y * width) * 4
 			const n = noise.octavate(5, x / 100, y / 100)
-			const g = (128 + Math.floor(128 * n)) * 4
+			const g = (128 + Math.floor(128 * n))
 
-			data[i] = gradientData[g]
-			data[i + 1] = gradientData[g + 1]
-			data[i + 2] = gradientData[g + 2]
+			data[i] = g
+			data[i + 1] = g
+			data[i + 2] = g
 			data[i + 3] = 255
 		}
 	}
 
 	const image = new ImageData(data, width, height)
 	ctx.putImageData(image, 0, 0)
+
+	gradientMap(canvas, {
+		0: 'blue',
+		0.5: 'blue',
+		0.51: 'black',
+		0.52: 'green',
+		1: 'green',
+	})
 }} width={400} height={400} />
