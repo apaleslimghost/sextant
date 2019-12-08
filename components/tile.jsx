303558e12360aaa389b77a221aaaa45bec1fa570
bren <bren@153.io>
2019-12-08 20:14:18 +0000

    gradient

--- components/tile.jsx
+++ components/tile.jsx
@@ -2,6 +2,22 @@ import React from 'react'
 import Canvas from './canvas'
 import { Simplex2 } from 'tumult'
 
+const gradientCanvas = document.createElement('canvas')
+gradientCanvas.width = 255
+gradientCanvas.height = 1
+
+const gradientCtx = gradientCanvas.getContext('2d')
+const gradient = gradientCtx.createLinearGradient(0, 0, 255, 0)
+gradient.addColorStop(0, 'blue')
+gradient.addColorStop(0.5, 'blue')
+gradient.addColorStop(0.501, 'green')
+gradient.addColorStop(1, 'green')
+
+gradientCtx.fillStyle = gradient
+gradientCtx.fillRect(0, 0, 255, 1)
+
+const gradientData = gradientCtx.getImageData(0, 0, 255, 1).data
+
 export default ({ }) => <Canvas draw={(ctx, canvas) => {
 	const noise = new Simplex2('seed')
 	const { width, height } = canvas
@@ -12,16 +28,15 @@ export default ({ }) => <Canvas draw={(ctx, canvas) => {
 		for (let x = 0; x < width; x++) {
 			const i = (x + y * width) * 4
 			const n = noise.octavate(5, x / 100, y / 100)
-			const g = 128 + Math.floor(128 * n)
+			const g = (128 + Math.floor(128 * n)) * 4
 
-			data[i] = g
-			data[i + 1] = g
-			data[i + 2] = g
+			data[i] = gradientData[g]
+			data[i + 1] = gradientData[g + 1]
+			data[i + 2] = gradientData[g + 2]
 			data[i + 3] = 255
 		}
 	}
 
 	const image = new ImageData(data, width, height)
 	ctx.putImageData(image, 0, 0)
-
 }} width={400} height={400} />
